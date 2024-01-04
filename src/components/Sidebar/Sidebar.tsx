dbc9a7b3dff15b430ca520a6b83bb8c5a10ba528
Owain <owain.evans@hotmail.co.uk>
2024-01-04 22:21:20 +0000

    sidebar creation and styling

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import "./Sidebar.scss";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+interface SidebarProps {
+  isCollapsed: boolean;
+  onToggleCollapse: () => void;
+  rows: number;
+  columns: number;
+  onRowsChange: (newRows: number) => void;
+  onColumnsChange: (newColumns: number) => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+  isCollapsed,
+  onToggleCollapse,
+  rows,
+  columns,
+  onRowsChange,
+  onColumnsChange,
+}) => {
+  return (
+    <div className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
+      <button
+        onClick={onToggleCollapse}
+        className={`burger-icon ${isCollapsed ? "" : "open"}`}
+      ></button>
+      {isCollapsed ? null : (
+        <div className="sidebar-items-container">
+          <label htmlFor="rows">Rows</label>
+          <input
+            name="rows"
+            type="number"
+            min="1"
+            max="10"
+            value={rows}
+            onChange={(e) => onRowsChange(Number(e.target.value))}
+          />
+          <label htmlFor="columns">Columns</label>
+          <input
+            name="columns"
+            type="number"
+            min="1"
+            max="10"
+            value={columns}
+            onChange={(e) => onColumnsChange(Number(e.target.value))}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
