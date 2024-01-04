69091bf9508c1e432f6e20fac3d4d7c4e3f5971f
Owain <owain.evans@hotmail.co.uk>
2024-01-04 22:21:56 +0000

    grid and styling

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Viewport from "../Viewport/Viewport";
+import "./Grid.scss";
+interface GridProps {
+  rows: number;
+  columns: number;
+}
 
-const Grid = () => {
-  return <div>Grid</div>;
+const Grid = ({ rows, columns }: GridProps) => {
+  // Create an array of row indices
+  const rowIndices = Array.from({ length: rows }, (_, index) => index);
+
+  // Create an array of column indices
+  const columnIndices = Array.from({ length: columns }, (_, index) => index);
+
+  return (
+    <div className="grid">
+      {columnIndices.map((columnIndex) => (
+        <div key={columnIndex} className="grid-column">
+          {rowIndices.map((rowIndex) => (
+            <Viewport key={rowIndex} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Grid;
